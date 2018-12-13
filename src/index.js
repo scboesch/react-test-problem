faaf0091745b8c1d7bdcc1e7604836ed01e14886
Chris Boesch <ChrisBoesch@nus.edu.sg>
2018-12-13 17:08:28 +0800

    Adding button to default app

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ export default class Button extends React.Component {
 function App() {
   return (
     <div className="App">
+      <Button count={1} />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
